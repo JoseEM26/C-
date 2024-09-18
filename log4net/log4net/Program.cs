823b84cf7b020e7c2032aa6784617ff1350e8636
JoseEM <joseangelespinozamorales@gmail.com>
2024-09-18 00:05:16 -0500

    first commit

---
+++ log4net/log4net/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace log4net
+{
+
+    internal class Program
+    {
+        private static readonly ILog LOG = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+
+            LOG.Info("bienvenidos a log4net");
+            
+
+                int numero = 10;
+                int denominador = 0;
+
+            try
+            {
+
+                if (numero == 10)
+                {
+                    throw new Exception("error Exception");
+                }
+
+            }catch(Exception e)
+            {
+                LOG.Error(e);
+                LOG.Error(e.Message);
+                Console.WriteLine(e.ToString());
+               
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+//aca puedes encontrar los datos necesarios
+//https://notepad.pw/KEB5J0c6HMLmf9Ks6IV9
