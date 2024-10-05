031de1433325201621793cfb198bc42dff77bdec
JoseEM <joseangelespinozamorales@gmail.com>
2024-10-04 21:56:35 -0500

    MENSAJITO

--- Tarea_GuiaMVC/SQLQuery2.sql
+++ Tarea_GuiaMVC/SQLQuery2.sql
@@ -28,3 +28,61 @@ END
 
 EXEC USP_CLIENTE_LISTAR
 EXEC USP_PEDIDO_LISTAR
+
+-----------------------------------------------------
+-----------------------------------------------------
+
+CREATE OR ALTER PROC USP_PRODUCTO_LISTAR_NOMBRE
+@NOMBRE VARCHAR(20)
+AS
+BEGIN
+   SELECT P.IdProducto ,
+          P.NomProducto,
+		  P.PrecioUnidad,
+		  C.NombreCategoria,
+		  S.NomProveedor,
+		  P.UnidadesEnExistencia AS [Stock]
+   FROM Productos AS P
+   INNER JOIN Proveedores AS S ON S.IdProveedor =P.IdProveedor
+   INNER JOIN Categorias AS C ON C.IdCategoria =P.IdCategoria
+   WHERE P.NomProducto LIKE '%'+@NOMBRE+'%' OR @NOMBRE=''
+END
+
+EXEC USP_PRODUCTO_LISTAR_NOMBRE 'QUESO'
+
+
+CREATE OR ALTER PROC USP_PRODUCTO_CATEGORIA
+@NOMBRECategoria VARCHAR(100)
+AS
+BEGIN
+   SELECT P.IdProducto ,
+          P.NomProducto,
+		  P.PrecioUnidad,
+		  C.NombreCategoria,
+		  S.NomProveedor,
+		  P.UnidadesEnExistencia AS [Stock]
+   FROM Productos AS P
+   INNER JOIN Proveedores AS S ON S.IdProveedor =P.IdProveedor
+   INNER JOIN Categorias AS C ON C.IdCategoria =P.IdCategoria
+   WHERE C.NombreCategoria  like '%'+@NOMBRECategoria+'%' or @NOMBRECategoria=''
+END
+
+
+exec USP_PRODUCTO_CATEGORIA 'bebidas'
+
+
+
+
+CREATE PROC USP_PRODUCTOlISTAR
+AS
+BEGIN
+  SELECT P.IdProducto ,
+          P.NomProducto,
+		  P.PrecioUnidad,
+		  C.NombreCategoria,
+		  S.NomProveedor,
+		  P.UnidadesEnExistencia AS [Stock]
+  FROM Productos AS P
+  INNER JOIN Categorias AS C ON C.IdCategoria=P.IdCategoria
+  INNER JOIN Proveedores AS S ON P.IdProveedor=S.IdProveedor
+  END
